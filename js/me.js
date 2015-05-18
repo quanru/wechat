14f384216f57a23df709a2de411c3fb685aabc71
quanru <quanruzhuoxiu@gmail.com>
2015-05-18 11:19:49 +0800

    增加视频聊天按钮与个人信息页面

--- js/me.js
+++ js/me.js
@@ -1,4 +1,7 @@
 $(function  () {
 	$("#addBtn").bind("click", clickDown);
 	$("#searchBtn").bind("click", clickDown);
+	$("#userName").bind("click", function  (e) {
+		window.location = "personality.html"
+	});
 });
--- js/person.js
+++ js/person.js
@@ -1,12 +1,14 @@
 var search = window.location.search.split('&');
 var userName = decodeURIComponent(search[0].substring(10));
-var imgSrc = decodeURIComponent(search[1].substring(8));
+var imgSrc = decodeURIComponent(search[1].substring(7));
 $(function  () {
 	$("#userName").html(userName);
-	$("#userBox img").attr('src', '.' + imgSrc);
+	$("#userBox img").attr('src', imgSrc);
 	$("#userBox img").bind("click", zoomIn);
 	$("#shade, #zoomImg").bind("click", getOut);//点击背景退出菜单
-	$("#sendBtn").bind("click",sendMessage);
+	$("#sendBtn").bind("click", sendMessage);//点击发消息按钮
+	$("#visionChat").bind("click", visionChat);//点击视频聊天
+	$("#menuBtn").bind("click", slideMenu);//点击右上角菜单
 });
 
 function zoomIn (event) {
@@ -18,6 +20,7 @@ function zoomIn (event) {
 		width:'480px',
 		height:'480px'
 	});
+	$("#shade").css("backgroundColor", "rgba(0,0,0,1)");
 	$("#shade").css("display", "block");
 }
 function getOut (event) {
@@ -34,4 +37,13 @@ function getOut (event) {
 function sendMessage (event) {
 	var url = "chatRoom.html?userName=" + encodeURIComponent(userName) + "&imgSrc=" + encodeURIComponent(imgSrc);
 	window.location = url;
+}
+function visionChat (event) {
+	$("#shade").css("backgroundColor", "rgba(0,0,0,0.5)");
+	$("#shade").fadeIn("slow");
+	$("#popMenu").fadeIn("slow");
+}
+function slideMenu (event) {
+	$("#slideMenu ul li:last-child img").attr("src", imgSrc);
+	$("#slideMenu").slideToggle("slow");
 }
